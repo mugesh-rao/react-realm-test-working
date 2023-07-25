43e76e4a2585936a22b814665b220f6c568c0543
MUgesh Rao <mugeshraoego@gmail.com>
2023-07-25 11:51:08 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {StyleSheet, View, ActivityIndicator} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {AppProvider, UserProvider} from '@realm/react';
+
+import {appId, baseUrl} from '../atlasConfig.json';
+import {WelcomeView} from './WelcomeView';
+import {ItemListView} from './ItemListView';
+import {realmContext} from './RealmContext';
+
+const {RealmProvider} = realmContext;
+
+const Stack = createStackNavigator();
+
+const AppWrapper = () => {
+  return (
+    <AppProvider id={appId} baseUrl={baseUrl}>
+      <UserProvider fallback={WelcomeView}>
+        <App />
+      </UserProvider>
+    </AppProvider>
+  );
+};
+
+const LoadingIndicator = () => {
+  return (
+    <View style={styles.activityContainer}>
+      <ActivityIndicator size="large" />
+    </View>
+  );
+};
+
+const App = () => {
+  return (
+    <>
+      <RealmProvider
+        sync={{
+          flexible: true,
+          onError: (_, error) => {
+            // Show sync errors in the console
+            console.error(error);
+          },
+        }}
+        fallback={LoadingIndicator}>
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen
+                name="Your To-Do List"
+                component={ItemListView}
+                options={{
+                  headerTitleAlign: 'center',
+                }}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </RealmProvider>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  activityContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
+  },
+});
+
+export default AppWrapper;
